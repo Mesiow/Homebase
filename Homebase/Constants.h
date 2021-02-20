d84624fb5e30842150fe050f164bbc27d78b6631
Chris <34993144+Mesiow@users.noreply.github.com>
2021-02-19 20:23:14 -0500

    Add arena constants

--- Homebase/Constants.h
+++ Homebase/Constants.h
@@ -2,3 +2,6 @@
 
 constexpr static float SCREEN_WIDTH = 1920;
 constexpr static float SCREEN_HEIGHT = 1080;
+
+constexpr static float ARENA_WIDTH = 4000;
+constexpr static float ARENA_HEIGHT = 4000;
